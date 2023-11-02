a3e83df2b060671939d9d8b4259ef86546244ac7
Amad Ul Hassan <48374522+AmadHashmi@users.noreply.github.com>
2023-11-03 00:12:28 +0100

    Updated
    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './model/employee';
+import { EmployeeService } from './service/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EmployeeManagerApp';
+export class AppComponent implements OnInit{
+  public employees: Employee[] | undefined;
+  
+  constructor(private employeeService: EmployeeService){
+
+  }
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees():void{
+    this.employeeService.getEmployees().subscribe((response: Employee[])=>{
+      this.employees = response
+    },
+    (error: Error)=>{
+      alert(error.message);
+    })
+  }
 }
